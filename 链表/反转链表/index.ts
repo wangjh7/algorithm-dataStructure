e3862b2087871e98dfd6a9857fe50e5203c41c30
johann <584971501@qq.com>
2022-11-10 17:09:17 +0800

    反转链表

---
+++ 链表/反转链表/index.ts
@@ -0,0 +1,55 @@
+
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
+//双指针
+function reverseList(head: ListNode | null): ListNode | null {
+  let prevNode : ListNode | null = null
+  let curNode : ListNode | null = head
+  let tempNode : ListNode | null
+  while(curNode) {
+    tempNode = curNode.next
+    curNode.next = prevNode
+    prevNode = curNode
+    curNode = tempNode
+  }
+  return prevNode
+};
+
+//递归 从前往后翻转
+function reverseList1(head: ListNode | null): ListNode | null {
+  function recur(prevNode:ListNode | null, curNode: ListNode | null) : ListNode | null{
+    if(curNode === null) {
+      return prevNode
+    }
+    let tempNode : ListNode | null = curNode.next
+    curNode.next = prevNode
+    prevNode = curNode
+    curNode = tempNode
+    return recur(prevNode,curNode)
+  }
+  return recur(null,head)
+}
+
+//递归 从后往前翻转
+let newHead : ListNode
+function reverseList2(head: ListNode | null): ListNode | null {
+  if(head === null) return null
+  function recur(node:ListNode,prevNode:ListNode | null) : void{
+    if(node.next === null){
+      newHead = node
+      newHead.next = prevNode
+    } else {
+      recur(node.next,node)
+      node.next = prevNode
+    }
+  }
+  recur(head,null)
+  return newHead
+}
