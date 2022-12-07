ed23ec60886c605fc6c85842ce25361e69a7e39e
johann <584971501@qq.com>
2022-12-07 11:17:07 +0800

    二叉树的最大深度

---
+++ 二叉树/二叉树的最大深度/index.ts
@@ -0,0 +1,44 @@
+import {TreeNode} from '../TreeNode'
+
+//迭代法
+//层序遍历
+function maxDepth(root: TreeNode | null): number {
+  let helperQueue:TreeNode[] = []
+  let resDepth:number = 0
+  let tempNode:TreeNode
+  if(root !== null) helperQueue.push(root)
+  while(helperQueue.length > 0){
+    resDepth++
+    let length = helperQueue.length
+    for(let i = 0; i < length; i++) {
+      tempNode = helperQueue.shift()!
+      if(tempNode.left) helperQueue.push(tempNode.left)
+      if(tempNode.right) helperQueue.push(tempNode.right)
+    }
+  }
+  return resDepth
+}
+
+
+//递归法
+//后序遍历
+function maxDepth(root: TreeNode | null): number {
+  if(root === null) return 0
+  return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1
+}
+
+//前序遍历
+function maxDepth(root: TreeNode | null): number {
+  function recur(node:TreeNode|null,count:number){
+    if(node === null) {
+      resMax = resMax > count ? resMax : count
+      return
+    }
+    recur(node.left,count+1)
+    recur(node.right,count+1)
+  }
+  let resMax:number = 0 
+  let count:number = 0
+  recur(root,count)
+  return resMax
+}
